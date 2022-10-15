4a5b2e6c5d44611ca6cde7614b309c5a12004cd2
Melvin Batista <mbatista@infrasystpr.com>
2022-10-14 23:15:15 -0400

    attribute binding

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 const app = Vue.createApp({
   data() {
     return {
+      url: "http://www.infrasyst.com",
       showBooks: true,
       books: [
-        { title: "name of the wind", author: "patrick rothfuss" },
-        { title: "the way of kings", author: "brandon sanderson" },
-        { title: "the final empire", author: "brandon sanderson" },
+        {
+          title: "name of the wind",
+          author: "patrick rothfuss",
+          img: "assets/1.jpg",
+        },
+        {
+          title: "the way of kings",
+          author: "brandon sanderson",
+          img: "assets/2.jpg",
+        },
+        {
+          title: "the final empire",
+          author: "brandon sanderson",
+          img: "assets/3.jpg",
+        },
       ],
     };
   },
